c986c0d9fbbceed39b050cffb836d1a3007d3a0a
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-06-26 13:42:06 +0600

    added auto admin privilege

--- Install_Alt_Run-As-Admin.cmd
+++ Install_Alt_Run-As-Admin.cmd
@@ -1,4 +1,15 @@
 @echo off
+
+FLTMC >NUL 2>&1 || (
+	ECHO SET UAC = CreateObject^("Shell.Application"^) > "%TEMP%\GetAdmin.vbs"
+	ECHO UAC.ShellExecute "%~FS0", "", "", "runas", 1 >> "%TEMP%\GetAdmin.vbs"
+	CMD /U /C TYPE "%TEMP%\GetAdmin.vbs">"%TEMP%\GetAdminUnicode.vbs"
+	CSCRIPT //NOLOGO "%TEMP%\GetAdminUnicode.vbs"
+	DEL /F /Q "%TEMP%\GetAdmin.vbs" >NUL 2>&1
+	DEL /F /Q "%TEMP%\GetAdminUnicode.vbs" >NUL 2>&1
+	EXIT
+)
+
 SET "desktop=%userprofile%\Desktop"
 
 echo.
